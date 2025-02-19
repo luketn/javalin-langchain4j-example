04d96ecfabd269c19a8996518d5cead9d603c82f
luketn <endeavour9@gmail.com>
2025-02-19 15:16:50 +1100

    try streaming to sse (didn't work immediately for my client)

--- src/main/java/com/mycodefu/Main.java
+++ src/main/java/com/mycodefu/Main.java
@@ -1,16 +1,24 @@
 package com.mycodefu;
 
 import io.javalin.Javalin;
+import io.javalin.http.sse.SseClient;
 import io.javalin.http.staticfiles.Location;
+
+import java.util.concurrent.ConcurrentLinkedQueue;
 
 import static io.javalin.apibuilder.ApiBuilder.get;
 
 public class Main {
     public static void main(String[] args) {
-        Javalin app = Javalin.create(config -> {
+        var clients = new ConcurrentLinkedQueue<SseClient>();
+        Javalin.create(config -> {
                     config.staticFiles.add("/public", Location.CLASSPATH);
                 })
-                .get("/stream", ctx -> ctx.result("Hello World"))
+                .sse("/stream", client -> {
+                    clients.add(client);
+                    client.sendEvent("Hekki");
+                    client.onClose(() -> clients.remove(client));
+                })
                 .start(7070);
     }
 }
