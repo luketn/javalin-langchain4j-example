6c831238888a2013add58836513d02d3752768e5
luketn <endeavour9@gmail.com>
2025-02-19 18:12:44 +1100

    working SSE streaning chat from post fetch!

--- src/main/java/com/mycodefu/Main.java
+++ src/main/java/com/mycodefu/Main.java
@@ -1,24 +1,57 @@
 package com.mycodefu;
 
+import dev.langchain4j.model.anthropic.AnthropicChatModelName;
+import dev.langchain4j.model.anthropic.AnthropicStreamingChatModel;
+import dev.langchain4j.model.chat.StreamingChatLanguageModel;
+import dev.langchain4j.model.chat.response.ChatResponse;
+import dev.langchain4j.model.chat.response.StreamingChatResponseHandler;
 import io.javalin.Javalin;
 import io.javalin.http.sse.SseClient;
+import io.javalin.http.sse.SseHandler;
 import io.javalin.http.staticfiles.Location;
 
 import java.util.concurrent.ConcurrentLinkedQueue;
 
-import static io.javalin.apibuilder.ApiBuilder.get;
-
 public class Main {
+    public record EventData(String message) { }
     public static void main(String[] args) {
         var clients = new ConcurrentLinkedQueue<SseClient>();
+
+        StreamingChatLanguageModel model = AnthropicStreamingChatModel.builder()
+                .apiKey(System.getenv("ANTHROPIC_API_KEY"))
+                .modelName(AnthropicChatModelName.CLAUDE_3_5_SONNET_20241022)
+                .build();
+
         Javalin.create(config -> {
                     config.staticFiles.add("/public", Location.CLASSPATH);
                 })
-                .sse("/stream", client -> {
-                    clients.add(client);
-                    client.sendEvent("Hekki");
-                    client.onClose(() -> clients.remove(client));
-                })
+                .post("/stream", new SseHandler(sseClient -> {
+                    sseClient.keepAlive();
+
+                    clients.add(sseClient);
+                    sseClient.onClose(() -> clients.remove(sseClient));
+
+                    String message = sseClient.ctx().queryParam("message");
+
+                    sseClient.sendEvent("init", new EventData(message));
+
+                    model.chat(message, new StreamingChatResponseHandler() {
+                        @Override
+                        public void onPartialResponse(String s) {
+                            sseClient.sendEvent("message", new EventData(s));
+                        }
+
+                        @Override
+                        public void onCompleteResponse(ChatResponse chatResponse) {
+                            sseClient.sendEvent("complete", new EventData("Done!"));
+                        }
+
+                        @Override
+                        public void onError(Throwable error) {
+                            sseClient.sendEvent("error", new EventData(error.getMessage()));
+                        }
+                    });
+                }))
                 .start(7070);
     }
 }
